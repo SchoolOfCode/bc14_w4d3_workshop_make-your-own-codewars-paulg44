75ae3fdbcc6b3ba2e199c9751a476a91b186cce1
paulg44 <paulgarton1984@hotmail.com>
2023-04-07 16:22:32 +0100

    update instructions

--- main.js
+++ main.js
@@ -2,9 +2,8 @@
 👉 Write your kata here!
 
    Chicken Chaos!!!
-   Someone left the gate to the chicken coop open over night (probably Arshi), a number of chickens have escaped and are scattered across the School of Code farm. 
+   Someone left the gate to the chicken coop open over night, a number of chickens have escaped and are scattered across the School of Code farm. 
    The missing chickens have been located and put into an array already for you, but there are numerous chickens in various locations, your task is to collect the missing chickens all in one place (single number) so they can be returned to the coop.......as a string!!!
-
    Example [1, 6, 6, 2] should return "15"
 */
 
