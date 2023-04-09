e71aa1ff5ae9de18f9dddf488d8b641cd6b1340b
paulg44 <paulgarton1984@hotmail.com>
2023-04-09 20:49:06 +0100

    simplify array variable in tests

--- main.test.js
+++ main.test.js
@@ -1,6 +1,6 @@
 //👉 Write your tests below here:
 import { test, expect } from "@jest/globals";
-import { chickensToReturn, missingChickens } from "./main.js";
+import { chickensToReturn } from "./main.js";
 
 // Test Ideas
 /* 
@@ -20,38 +20,42 @@ test("returned string", function () {
 });
 
 // Array Tests
+
+const missingChickens = [
+  [1, 6, 6, 2],
+  [7, 2, 3, 1],
+  [2, 9, 10, 6],
+  [4, 4, 3],
+  [1, 6, 6, 2, 9],
+];
+
 test("returned chickens test one", function () {
-  const testCaseOne = [1, 6, 6, 2];
-  const actual = chickensToReturn(testCaseOne);
+  const actual = chickensToReturn(missingChickens[0]);
   const expected = "15";
 
   expect(actual).toMatch(expected);
 });
 
 test("returned chickens test two", function () {
-  const testCaseTwo = [7, 2, 3, 1];
-  const actual = chickensToReturn(testCaseTwo);
+  const actual = chickensToReturn(missingChickens[1]);
   const expected = "13";
 
   expect(actual).toMatch(expected);
 });
 test("returned chickens test three", function () {
-  const testCaseThree = [2, 9, 10, 6];
-  const actual = chickensToReturn(testCaseThree);
+  const actual = chickensToReturn(missingChickens[2]);
   const expected = "27";
 
   expect(actual).toMatch(expected);
 });
 test("returned chickens test four", function () {
-  const testCaseFour = [4, 4, 3];
-  const actual = chickensToReturn(testCaseFour);
+  const actual = chickensToReturn(missingChickens[3]);
   const expected = "11";
 
   expect(actual).toMatch(expected);
 });
 test("returned chickens test five", function () {
-  const testCaseFive = [1, 6, 6, 2, 9];
-  const actual = chickensToReturn(testCaseFive);
+  const actual = chickensToReturn(missingChickens[4]);
   const expected = "24";
 
   expect(actual).toMatch(expected);
